62a28dfc7e13867ad46ff5c4ba43055adc02eb10
Yuan Pan <y39pan@edu.uwaterloo.ca>
2018-08-05 19:05:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value){
+  var newObject = newObject()
   object[key] = value;
   return object;
 }
-
-function updateObjectWithKeyAndValue 
